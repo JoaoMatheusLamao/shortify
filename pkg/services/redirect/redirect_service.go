69516143c4937221fd46e426630502a7d06d0e69
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2025-01-04 20:31:54 -0300

    feat: insert new analytcis in mongo

--- pkg/services/redirect/redirect_service.go
+++ pkg/services/redirect/redirect_service.go
@@ -2,10 +2,15 @@ package redirect
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"shortify/pkg/config"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // FindOriginalURLAndRedirect is a function that redirects the user to the original URL
@@ -25,5 +30,31 @@ func FindOriginalURLAndRedirect(cfg *config.Config) gin.HandlerFunc {
 		}
 
 		c.Redirect(302, originalURL)
+
+		go insertAnalyticsInMongo(originalURL, c.ClientIP(), cfg)
+	}
+}
+
+// insertAnalyticsInMongo is a function that inserts the analytics data in MongoDB
+func insertAnalyticsInMongo(originalURL, clientIP string, cfg *config.Config) {
+
+	collection := cfg.Mongo.Database("shortify").Collection("short_urls")
+
+	filter := bson.D{{Key: "original_url", Value: originalURL}}
+	update := bson.D{
+		{Key: "$set", Value: bson.D{
+			{Key: "last_find", Value: time.Now()},
+		}},
+		{Key: "$inc", Value: bson.D{
+			{Key: "counter_find", Value: 1},
+			{Key: fmt.Sprintf("ip_counter_find.%s", clientIP), Value: 1},
+		}},
+	}
+
+	opts := options.Update().SetUpsert(true)
+
+	_, err := collection.UpdateOne(context.Background(), filter, update, opts)
+	if err != nil {
+		log.Println("Error updating analytics in the MongoDB database: " + err.Error())
 	}
 }
