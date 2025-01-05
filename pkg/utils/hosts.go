bbdcff494a45ce1786ac1649c621ea7a7bca0a8e
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2025-01-04 23:07:00 -0300

    fix: check tls

--- pkg/utils/hosts.go
+++ pkg/utils/hosts.go
@@ -1,11 +1,15 @@
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 // GetCurrentProtocolAndHost returns the current protocol and host
 func GetCurrentProtocolAndHost(c *gin.Context) string {
 	protocol := "http"
-	if c.Request.TLS != nil {
+	if os.Getenv("SERVER_TLS") == "true" {
 		protocol = "https"
 	}
 	host := c.Request.Host
